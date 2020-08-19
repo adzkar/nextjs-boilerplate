456e1b5a95d374bf1b073d381bbaa6f8d2378498
Adzkar Fauzie <adzkarfauzie02@gmail.com>
2020-08-19 11:15:58 +0700

    feat: add sample containers

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const withImages = require("next-images");
+const withImages = require('next-images');
 
 module.exports = withImages({
   webpack(config, options) {
     return config;
   },
-  distDir: "build",
+  distDir: 'build',
 });
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,3 @@
 export default {
-  pikachu: require("./pikachu.gif"),
+  pikachu: require('./pikachu.gif'),
 };
